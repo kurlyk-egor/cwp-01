b99ec7ebdae48b5d2f8a8d20c66db913c18770c6
kurlyk-egor <vikingegor@gmail.com>
2017-11-08 00:34:55 +0300

    summary string version

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-function getFiles(dir) { 
-    fs.readdir(dir, function (err, files) { 
-        for (let i in files) { 
-            let currentDir = dir + path.sep + files[i]; 
+let summary = 
+'const fs = require(\'fs\');\n'+
+'const path = require(\'path\');\n'+
+'function getFiles(dir) { \n'+
+'    fs.readdir(dir, function (err, files) {\n'+
+'        for (let i in files) { \n'+
+'            let currentDir = dir + path.sep + files[i];\n '+
+ '           fs.stat(currentDir, (err, stats) => { \n'+
+ '               if (stats.isDirectory()) { \n'+
+ '                   getFiles(currentDir);\n '+
+ '                   return;\n'+
+ '               }\n'+
+ '               console.log(path.relative(__dirname, currentDir));\n '+
+ '           });\n '+
+ '       }\n'+
+ '   });\n'+
+'};\n'+
+'getFiles(__dirname);\n';
 
-            fs.stat(currentDir, (err, stats) => { 
-                if (stats.isDirectory()) { 
-                    getFiles(currentDir); 
-                    return;
-                }
-                console.log(path.relative(__dirname, currentDir)); 
-            }); 
-        }
-    });
-};
-
-getFiles(__dirname);
+console.log(summary);
