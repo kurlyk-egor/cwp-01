dd57dccbd6e13616d5d9181998dddba95ad7d55d
kurlyk-egor <vikingegor@gmail.com>
2017-11-08 00:29:54 +0300

    summary done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const name = process.argv[2];
+const fs = require('fs');
+const path = require('path');
 
-process.argv.forEach(function(element) {
-    console.log(element);
-}, this);
+function getFiles(dir) { 
+    fs.readdir(dir, function (err, files) { 
+        for (let i in files) { 
+            let currentDir = dir + path.sep + files[i]; 
+
+            fs.stat(currentDir, (err, stats) => { 
+                if (stats.isDirectory()) { 
+                    getFiles(currentDir); 
+                    return;
+                }
+                console.log(path.relative(__dirname, currentDir)); 
+            }); 
+        }
+    });
+};
+
+getFiles(__dirname);
