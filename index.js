9f43ceff305bcbdec247ae6418cc646223c04634
kurlyk-egor <vikingegor@gmail.com>
2017-11-08 04:00:06 +0300

    final

--- index.js
+++ index.js
@@ -1,60 +1,123 @@
-const fs = require('fs');
 const path = require('path');
-const txt = 'txt';
+const fs = require('fs');
+
+const DIR = process.argv[2];
+const TXT = '.txt';
+
 let copyright;
+let summary =
+    'const fs = require(\'fs\');\n' +
+    'const path = require(\'path\');\n' +
+    '\n' +
+    '(function getFiles(baseDir) {\n' +
+    '    fs.readdir(baseDir, function (err, files){\n' +
+    '        for (let i in files) {\n' +
+    '            let currentDir = baseDir + path.sep + files[i];\n' +
+    '            fs.stat(currentDir, (err, stats) => {\n' +
+    '                    if (stats.isDirectory()) {\n' +
+    '                        getFiles(currentDir);\n' +
+    '                    } else {\n' +
+    '                        console.log(path.relative(__dirname, currentDir));\n' +
+    '                    }\n' +
+    '                }\n' +
+    '            );\n' +
+    '        }\n' +
+    '    });\n' +
+    '})(__dirname);';
 
-let summary = 
-'const fs = require(\'fs\');\n'+
-'const path = require(\'path\');\n'+
-'function getFiles(dir) { \n'+
-'    fs.readdir(dir, function (err, files) {\n'+
-'        for (let i in files) { \n'+
-'            let currentDir = dir + path.sep + files[i];\n '+
- '           fs.stat(currentDir, (err, stats) => { \n'+
- '               if (stats.isDirectory()) { \n'+
- '                   getFiles(currentDir);\n '+
- '                   return;\n'+
- '               }\n'+
- '               console.log(path.relative(__dirname, currentDir));\n '+
- '           });\n '+
- '       }\n'+
- '   });\n'+
-'};\n'+
-'getFiles(__dirname);\n';
 
-function createSummaryScript() {
-    fs.appendFile(process.argv[2] + '\\summary.js', summary, (err) => {
-        if (err) {
-            console.log('summary.js not created');
+(() => {
+    fs.access(DIR, (err) => {
+            if (err) {
+                console.log(err);
+                console.log("path error");
+            }
+            else {
+
+                let txtDir = createTxtDir();
+                createSummary();
+                setCopyright();
+                createCopies(DIR, txtDir);
+                whatchDir(DIR);
+            }
         }
-    });
-}
+    )
+})();
 
 function setCopyright() {
     fs.readFile("config.json", (err, data) => {
         if (err) {
-            console.error("Not finded copyright file");
-            return;
+            console.error("failed set copyright");
+            copyright = 'undefined';
         }
-        copyright = JSON.parse(data);
-        console.log(copyright);
+        else {
+            copyright = JSON.parse(data);
+        }
     });
 }
 
-function createNewDir() {
-    let mypath = process.argv[2] + '\\' + path.basename(process.argv[2]);
-    fs.mkdir(mypath, (err) => {
-        if (err && err.code != 'EEXIST') {
-            console.log("error while creating dir");
+function createTxtDir() {
+    let dirPath = DIR + "\\" + path.basename(DIR);
+    fs.mkdir(dirPath, (err) => {
+        if (err) {
+            console.log("Make dir error");
             throw err;
         }
     });
-    return mypath;
+    return dirPath;
 }
 
-//createSummaryScript();
+function createSummary() {
 
-(() => {
-    setCopyright();
-    createNewDir();
-})();
+    fs.appendFile(DIR + "\\" + "summary.js", summary, (err) => {
+        if (err) {
+            console.log(err);
+            console.log('Failed to create summary.js');
+        }
+    });
+}
+
+function createCopies(dir, targetDir) {
+    fs.readdir(dir, function (err, files) {
+        if (err)
+            console.log(err);
+        else {
+            for (let file in files) {
+
+                let currentDir = dir + "\\" + files[file];
+
+                if (fs.statSync(currentDir).isDirectory()) {
+                    createCopies(currentDir, targetDir);
+                } else {
+                    if (path.extname(currentDir) === TXT) {
+                        fs.readFile(currentDir, 'utf8', (err, data) => {
+                            if (err) {
+                                console.log("Can't read file:" + currentDir);
+                            }
+                            else {
+                                writeDataToTxt(targetDir + path.sep + files[file], data);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    });
+}
+
+function writeDataToTxt(filePath, data) {
+
+    let appendText = copyright["copyright"] + data + copyright["copyright"];
+    fs.appendFile(filePath, appendText, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+            console.log("Failed writing file");
+        }
+    });
+}
+
+function whatchDir(dirForTxt) {
+    fs.watch(dirForTxt, (eventType, filename) => {
+        console.log("event " + eventType.toString() + " " + filename.toString());
+    });
+}
